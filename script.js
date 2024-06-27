b61c48b5c2826ea13d9472536f0c43b19040740d
Paddasravani-05 <145531329+Paddasravani-05@users.noreply.github.com>
2024-06-27 21:59:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,27 +42,3 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-  // const slides = document.querySelectorAll('.carousel-item');
-  // let currentIndex = 0;
-
-  // document.querySelector('.next').addEventListener('click', () => {
-  //   changeSlide(currentIndex + 1);
-  // });
-
-  // document.querySelector('.prev').addEventListener('click', () => {
-  //   changeSlide(currentIndex - 1);
-  // });
-
-  // function changeSlide(index) {
-  //   if (index >= slides.length) {
-  //     currentIndex = 0;
-  //   } else if (index < 0) {
-  //     currentIndex = slides.length - 1;
-  //   } else {
-  //     currentIndex = index;
-  //   }
-  //   const offset = -currentIndex * 100;
-  //   document.querySelector('.carousel-slide').style.transform = `translateX(${offset}%)`;
-  // }
- 
-  
